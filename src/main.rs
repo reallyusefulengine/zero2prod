a1202dd1bc55b967d5651bec8c3db060a88ece66
JW <jhopkinwilliams@yahoo.com>
2022-05-07 19:27:40 +1000

    complaining about linting

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{ web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -6,10 +6,10 @@ async fn greet(req: HttpRequest) -> impl Responder {
 }
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> { 
+async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet)) 
+            .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
