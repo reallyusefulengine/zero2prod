067c1a072d5c313058815527cff50a3682946ff7
reallyusefulengine <will.jh75@gmail.com>
2022-05-18 22:02:16 +1000

    added the subscribe tests and api endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,24 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String
+}
+
+async fn subscripe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 // Notice the different signature!
 // We return `Server` on the happy path and we dropped the `async` keyword // We have no .await call, so it is not needed anymore.
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+    let server = HttpServer::new(|| App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscripe))
+        )
         .listen(listener)?
         .run();
     // No .await here!
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 //! src/main.rs
-use zero2prod::run;
 use std::net::TcpListener;
+use zero2prod::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8000")?;
     run(listener)?.await
-}    
+}
